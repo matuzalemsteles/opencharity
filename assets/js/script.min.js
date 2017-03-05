031c27a442303a3caf23df2048fee2755e1b24e1
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-03-05 16:26:37 -0300

    Adding section blog

--- assets/js/script.min.js
+++ assets/js/script.min.js
@@ -2,6 +2,16 @@ $('.slider-members').owlCarousel({
   loop: false,
   items: 5,
   nav: false,
-  margin: 100,
-  bullets: true
+  margin: 100
+});
+
+$('.blog-slider').owlCarousel({
+  loop: false,
+  items: 4,
+  nav: true,
+  margin: 40,
+  dots: false,
+  navText: [
+  	'<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'
+  ]
 });
