f32b189ea26e2b4e38268e814a8b90ebdc4b7a1f
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-03-06 02:25:45 -0300

    Improving performance of the site

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require("gulp-concat");
 var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
@@ -19,6 +20,7 @@ gulp.task('sass', function () {
 gulp.task('minify-js', function () {
     gulp.src(js)
     .pipe(concat('script.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./assets/js/'));
 });
 
