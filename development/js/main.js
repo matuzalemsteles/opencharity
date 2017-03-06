78011b72d15bb500cac8a5f12cac4e6e68fd59cb
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-03-06 01:57:07 -0300

    Adding site responsive

--- development/js/main.js
+++ development/js/main.js
@@ -1,8 +1,32 @@
+$(document).ready(function () {
+    $(".navbar-toggle").click(function (e) {
+        $('.navbar-nav').closest('.navbar-nav').toggleClass("navbar-open");
+        e.stopPropagation();
+    });
+    $(document).on('click', function (e) {    
+        if (!$(e.target).closest('.navbar-open').length) $('.navbar-nav').removeClass("navbar-open");
+    });
+});
+
 $('.slider-members').owlCarousel({
   loop: false,
   items: 5,
   nav: false,
-  margin: 100
+  margin: 100,
+  responsive: {
+    0: {
+      items: 2
+    },
+    520: {
+      items: 3
+    },
+    700: {
+      items: 4
+    },
+    992: {
+      items: 5
+    }
+  }
 });
 
 $('.blog-slider').owlCarousel({
@@ -13,5 +37,19 @@ $('.blog-slider').owlCarousel({
   dots: false,
   navText: [
   	'<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'
-  ]
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    740: {
+      items: 2
+    },
+    870: {
+      items: 3
+    },
+    992: {
+      items: 4
+    }
+  }
 });
